ec0b8eaacb0782904c031fcda161e86e995b7d9b
tastytacos <charlesligne@gmail.com>
2019-06-08 18:43:24 +0300

    Added xmlns to resulting output xml file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from parser import parse
-from datetime import datetime
 
 input_file_location = "res/Radiation.txt"
 output_file_location = "res/output.xml"
--- parser.py
+++ parser.py
@@ -6,6 +6,13 @@ from measurements_handler import handle_measure_date, handle_measure_value
 import pandas as pd
 
 creation_time = datetime.now()
+xmlms = {'base': "http://www.iaea.org/2012/IRIX/Format/Base",
+         'html': "http://www.w3.org/1999/xhtml",
+         'id': "http://www.iaea.org/2012/IRIX/Format/Identification",
+         'irix': "http://www.iaea.org/2012/IRIX/Format",
+         'irmis': "http://iec.iaea.org/irmis/2014/irix/format/extensions",
+         'loc': "http://www.iaea.org/2012/IRIX/Format/Locations",
+         'mon': "http://www.iaea.org/2012/IRIX/Format/Measurements"}
 
 
 def get_excel_information(filename):
@@ -44,8 +51,8 @@ def handle_lines(lines):
 
 # Measurements and Locations block
 def to_xml(handled_lines, locations_data):
-    measurement_root = xml.Element("Measurements", ValidAt=str(creation_time))
-    locations_root = xml.Element("Locations")
+    measurement_root = xml.Element("mon:Measurements", ValidAt=str(creation_time))
+    locations_root = xml.Element("loc:Locations")
     for line in handled_lines:
         # the next checks are a requirement of specification
         if len(line.split(" ")) != 3:
@@ -58,23 +65,23 @@ def to_xml(handled_lines, locations_data):
         measure_date = handle_measure_date(line.split(" ")[1])
         measure_value = handle_measure_value(line.split(" ")[2])
 
-        measurement_results = xml.SubElement(measurement_root, "MeasurementResult")
-        dose_rate = xml.SubElement(measurement_results, "DoseRateType").text = "Gamma"
-        measurement_period = xml.SubElement(measurement_results, "MeasuringTime")
-        start_m_time = xml.SubElement(measurement_period, "StartTime").text = measure_date
-        measurements = xml.SubElement(measurement_results, "Measurements")
-        measurement = xml.SubElement(measurements, "Measurement")
-        measurement_location = xml.SubElement(measurement, "Location", station_index=station_index)
-        value_units = xml.SubElement(measurement, "Value", Unit="Sv/s").text = measure_value
-        validated = xml.SubElement(measurement, "Validated").text = "NotValidated"
+        measurement_results = xml.SubElement(measurement_root, "mon:DoseRate")
+        dose_rate = xml.SubElement(measurement_results, "mon:DoseRateType").text = "Gamma"
+        measurement_period = xml.SubElement(measurement_results, "mon:MeasuringPeriod")
+        start_m_time = xml.SubElement(measurement_period, "mon:StartTime").text = measure_date
+        measurements = xml.SubElement(measurement_results, "mon:Measurements")
+        measurement = xml.SubElement(measurements, "mon:Measurement")
+        measurement_location = xml.SubElement(measurement, "mon:Location", station_index=station_index)
+        value_units = xml.SubElement(measurement, "mon:Value", Unit="Sv/s").text = measure_value
+        validated = xml.SubElement(measurement, "mon:Validated").text = "NotValidated"
 
-        location = xml.SubElement(locations_root, "Location", id=station_index)
+        location = xml.SubElement(locations_root, "loc:Location", id=station_index)
         name = locations_data.get(station_index).get("name")
-        stantion_name = xml.SubElement(location, "Name").text = name
-        geo_coord = xml.SubElement(location, "GeographicCoordinates")
-        latitude = xml.SubElement(geo_coord, "Latitude").text = locations_data.get(station_index).get("latitude")
-        longitude = xml.SubElement(geo_coord, "Longitude").text = locations_data.get(station_index).get("longitude")
-        height = xml.SubElement(geo_coord, "Height", Above="Sea", Unit="m").text = locations_data.get(
+        stantion_name = xml.SubElement(location, "loc:Name").text = name
+        geo_coord = xml.SubElement(location, "loc:GeographicCoordinates")
+        latitude = xml.SubElement(geo_coord, "loc:Latitude").text = locations_data.get(station_index).get("latitude")
+        longitude = xml.SubElement(geo_coord, "loc:Longitude").text = locations_data.get(station_index).get("longitude")
+        height = xml.SubElement(geo_coord, "loc:Height", Above="Sea", Unit="m").text = locations_data.get(
             station_index).get("height")
 
     measurements_tree = xml.ElementTree(measurement_root)
@@ -82,8 +89,15 @@ def to_xml(handled_lines, locations_data):
     return measurements_tree, locations_tree
 
 
+def get_report_root():
+    root = xml.Element("irix:Report")
+    for key, value in xmlms.items():
+        root.attrib['xmlns:' + key] = value
+    return root
+
+
 def create_xml_doc(id_tree, measures_tree, locations_xml_tree):
-    report_root = xml.Element("Report")
+    report_root = get_report_root()
     result_tree = combine_xml(report_root, [id_tree, measures_tree, locations_xml_tree])
     report_root_tree = xml.ElementTree(result_tree)
     return report_root_tree
@@ -103,37 +117,38 @@ def format_time(given_time):
 
 # Identification block
 def create_id_xml():
-    id_root = xml.Element("Identification")
-    org_reporting = xml.SubElement(id_root, "OrganisationsReporting").text = "meteo.gov.ua"
+    id_root = xml.Element("id:Identification")
+    org_reporting = xml.SubElement(id_root, "id:OrganisationsReporting").text = "meteo.gov.ua"
     time = format_time(creation_time)
-    report_datetime = xml.SubElement(id_root, "DateAndTimeOfCreation").text = str(time)
-    report_context = xml.SubElement(id_root, "ReportContext").text = "Routine"
-    report_uuid = xml.SubElement(id_root, "ReportUUID").text = str(uuid.uuid4())
-    confidentiality = xml.SubElement(id_root, "Confidentiality").text = "For Authority Use Only"
-    identifications_fields = xml.SubElement(id_root, "Identifications")
+    report_datetime = xml.SubElement(id_root, "id:DateAndTimeOfCreation").text = str(time)
+    report_context = xml.SubElement(id_root, "id:ReportContext").text = "Routine"
+    report_uuid = xml.SubElement(id_root, "id:ReportUUID").text = str(uuid.uuid4())
+    confidentiality = xml.SubElement(id_root, "id:Confidentiality").text = "For Authority Use Only"
+    identifications_fields = xml.SubElement(id_root, "id:Identifications")
 
-    person_info = xml.SubElement(identifications_fields, "PersonContactInfo")
-    name = xml.SubElement(person_info, "Name").text = "Leonid Tabachnyi"
-    org_person_id = xml.SubElement(person_info, "OrganisationID").text = "tabachnyi@meteo.gov.ua"
-    email = xml.SubElement(person_info, "EmailAdress").text = "380442399353"
+    person_info = xml.SubElement(identifications_fields, "base:PersonContactInfo")
+    name = xml.SubElement(person_info, "base:Name").text = "Leonid Tabachnyi"
+    org_person_id = xml.SubElement(person_info, "base:OrganisationID").text = "tabachnyi@meteo.gov.ua"
+    email = xml.SubElement(person_info, "base:EmailAdress").text = "380442399353"
 
-    org_contacts_info = xml.SubElement(identifications_fields, "OrganisationContactInfo")
-    org_name = xml.SubElement(org_contacts_info, "Name").text = "Radiation Accidents Consequences Prediction Center"
-    org_id = xml.SubElement(org_contacts_info, "OrganisationInfo").text = "meteo.gov.ua"
-    org_country = xml.SubElement(org_contacts_info, "Country").text = "UA"
-    org_phone_number = xml.SubElement(org_contacts_info, "PhoneNumber").text = "380442399353"
-    org_fax_number = xml.SubElement(org_contacts_info, "FaxNumber").text = "380442796680"
-    org_email = xml.SubElement(org_contacts_info, "EmailAddress").text = "ceprac@meteo.gov.ua"
-    org_description = xml.SubElement(org_contacts_info, "Descrition").text = "Data originator for this report"
+    org_contacts_info = xml.SubElement(identifications_fields, "base:OrganisationContactInfo")
+    org_name = xml.SubElement(org_contacts_info,
+                              "base:Name").text = "Radiation Accidents Consequences Prediction Center"
+    org_id = xml.SubElement(org_contacts_info, "base:OrganisationInfo").text = "meteo.gov.ua"
+    org_country = xml.SubElement(org_contacts_info, "base:Country").text = "UA"
+    org_phone_number = xml.SubElement(org_contacts_info, "base:PhoneNumber").text = "380442399353"
+    org_fax_number = xml.SubElement(org_contacts_info, "base:FaxNumber").text = "380442796680"
+    org_email = xml.SubElement(org_contacts_info, "base:EmailAddress").text = "ceprac@meteo.gov.ua"
+    org_description = xml.SubElement(org_contacts_info, "base:Descrition").text = "Data originator for this report"
 
-    org1_contacts_info = xml.SubElement(identifications_fields, "OrganisationContactInfo")
-    org1_name = xml.SubElement(org1_contacts_info, "Name").text = "Ukrainian Hydrometeorological Center"
-    org1_id = xml.SubElement(org1_contacts_info, "OrganisationID").text = "meteo.gov.ua"
-    org1_country = xml.SubElement(org1_contacts_info, "Country").text = "UA"
-    org1_phone_number = xml.SubElement(org1_contacts_info, "PhoneNumber").text = "380442399387"
-    org1_fax_number = xml.SubElement(org1_contacts_info, "FaxNUmber").text = "380442791080"
-    org1_email = xml.SubElement(org1_contacts_info, "EmailAddress").text = "office@meteo.gov.ua"
-    org1_description = xml.SubElement(org1_contacts_info, "Description").text = "Data originator for this report"
+    org1_contacts_info = xml.SubElement(identifications_fields, "base:OrganisationContactInfo")
+    org1_name = xml.SubElement(org1_contacts_info, "base:Name").text = "Ukrainian Hydrometeorological Center"
+    org1_id = xml.SubElement(org1_contacts_info, "base:OrganisationID").text = "meteo.gov.ua"
+    org1_country = xml.SubElement(org1_contacts_info, "base:Country").text = "UA"
+    org1_phone_number = xml.SubElement(org1_contacts_info, "base:PhoneNumber").text = "380442399387"
+    org1_fax_number = xml.SubElement(org1_contacts_info, "base:FaxNUmber").text = "380442791080"
+    org1_email = xml.SubElement(org1_contacts_info, "base:EmailAddress").text = "office@meteo.gov.ua"
+    org1_description = xml.SubElement(org1_contacts_info, "base:Description").text = "Data originator for this report"
     id_tree = xml.ElementTree(id_root)
     return id_tree
 
