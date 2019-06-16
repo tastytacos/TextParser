b5cccd6e48a4a33b19c969ee9fb4dcb66f781a34
tastytacos <charlesligne@gmail.com>
2019-06-16 14:57:13 +0300

    Provided logging

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from parser import parse
+import logging
 
-input_file_location = "res/Radiation.txt"
+input_file_location = "res/Radiation1.txt"
 output_file_location = "res/output.xml"
 
 
 def start():
     xml_doc = parse(input_file_location)
+    logging.info("Successfully parsed the xml information from - {} file".format(input_file_location))
     xml_doc.write(output_file_location)
+    logging.info("Successfully created the - {} xml file".format(output_file_location))
 
 
 if __name__ == '__main__':
